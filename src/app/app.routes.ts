7232fce764d5200f7cc6a5317c34331b8513f84d
unknown <cedriccoulibaly13@gmail.com>
2024-05-02 01:52:29 +0200

    routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
+import { ServDevComponent } from './Front-end/Pages/serv-dev/serv-dev.component';
 import {LoginComponent} from "./Front-end/Auth/login/login.component";
+import { Form5Component } from './Front-end/Pages/serv-dev/modal5/form5/form5.component';
 
 export const routes: Routes = [
-  {path:'',
-    component:LoginComponent,
+  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent },
+ 
+  {path:'projetweb',
+  component:ServDevComponent,
   children:[
-    {path:'',redirectTo:'login',pathMatch:"full"},
-    {path:'login',component:LoginComponent}
-  ]
-  },
-  
+    {
+      path:'',
+      redirectTo:'/projetweb/form5',
+      pathMatch:'full',
+    },
+ 
+  ]}
+
 ];
