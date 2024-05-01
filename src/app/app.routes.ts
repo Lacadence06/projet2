b2c617e3806d7a4c2096cf325586579a6b4e6936
unknown <cedriccoulibaly13@gmail.com>
2024-05-01 15:27:15 +0200

    modification sur les routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,13 @@
 import { Routes } from '@angular/router';
-import {AuthLayoutsComponent} from "./Front-end/Layouts/auth-layouts/auth-layouts.component";
 import {LoginComponent} from "./Front-end/Auth/login/login.component";
-import {DashboardLayoutsComponent} from "./Front-end/Layouts/dashboard-layouts/dashboard-layouts.component";
-import {DashboardComponent} from "./Front-end/Pages/dashboard/dashboard.component";
-import {SoumisInfoComponent} from "./Front-end/Pages/soumis-info/soumis-info.component";
 
 export const routes: Routes = [
   {path:'',
-    component:AuthLayoutsComponent,
+    component:LoginComponent,
   children:[
     {path:'',redirectTo:'login',pathMatch:"full"},
     {path:'login',component:LoginComponent}
   ]
   },
-  {path:'',
-  component:DashboardLayoutsComponent,
-  children:[
-    {path: 'dashboard',component:DashboardComponent},
-    {path: 'soumis',component: SoumisInfoComponent},
-  ]
-  }
+  
 ];
