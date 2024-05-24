c0cb8154558c67ffb982b75d059469fef151fe88
MaurelHack <gossechristyvan@gmail.com>
2024-05-24 01:34:02 +0000

    ajout de composant

---
+++ src/app/Front-end/Pages/liste-demande/liste-demande.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-demande',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-demande.component.html',
+  styleUrl: './liste-demande.component.css'
+})
+export class ListeDemandeComponent {
+
+}
---
+++ src/app/Front-end/Pages/soumis-info/soumis-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soumis-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './soumis-info.component.html',
+  styleUrl: './soumis-info.component.css'
+})
+export class SoumisInfoComponent {
+
+}
